36127761917a3be356d210ce80b184707629302f
KyungminPark-steck <110165529+KyungminPark-steck@users.noreply.github.com>
2025-07-22 12:30:49 +0900

    Update dataclass.py

--- UnLoG_training/pretraining/dataclass.py
+++ UnLoG_training/pretraining/dataclass.py
@@ -77,61 +77,7 @@ class EnglishPretrainCorpus:
             e_idx += bsz_one_step
         print ('Number of dev batches is {}'.format(len(dev_inputs)))
         return dev_inputs, dev_labels
-    '''
-    ##################### 원본 함수 
-    def __iter__(self):
-        lines = self.stream.readlines(BUFFER_SIZE)
 
-        if not lines:
-            print ('----------------------------------------')
-            self.epoch_id += 1
-            print (self.epoch_id)
-            self.stream.close()
-            self.stream = open(self.train_path, encoding='utf8')
-            lines = self.stream.readlines(BUFFER_SIZE)
-
-        text_list = []
-        for l in lines:
-            # each line of the lines is a single document
-            text_list.append(l.strip('\n').strip())
-
-        # shuffle the document
-        random.shuffle(text_list)
-        # create single long text from the buffered lines
-        single_text = ''
-        for text in text_list:
-            single_text += text + self.eos_token
-        # tokenize the long text
-        str_ids = self.tokenizer.encode(single_text)
-        # split the long string ids into segments with predefined length
-        str_len = len(str_ids)
-        example_num = str_len // self.block_size
-        buffer_examples = []
-        start_idx, end_idx = 0, self.block_size
-        for _ in range(example_num):
-            one_buffer = str_ids[start_idx:end_idx]
-            assert len(one_buffer) == self.seqlen + 1
-            buffer_examples.append(one_buffer)
-            start_idx += self.block_size
-            end_idx += self.block_size
-        random.shuffle(buffer_examples)
-
-        # fetch batch data from buffer_examples
-        batch_num = example_num // self.bsz_one_step
-        assert batch_num > 0
-        idx = 0
-        s_idx, e_idx = 0, self.bsz_one_step
-        while idx < batch_num:
-            inputs, labels = [], []
-            for one_example_id in buffer_examples[s_idx:e_idx]:
-                inputs.append(one_example_id[:-1])
-                labels.append(one_example_id[1:])
-            assert len(inputs) == self.bsz_one_step
-            s_idx += self.bsz_one_step
-            e_idx += self.bsz_one_step
-            idx += 1
-            yield torch.LongTensor(inputs), torch.LongTensor(labels)
-    '''
     
     def load_data_from_csv(self):
         with open(self.train_path, 'r', encoding='utf8') as csvfile:
@@ -146,55 +92,7 @@ class EnglishPretrainCorpus:
 
         return anchor_list, anchor_plus_list, anchor_minus_list
     
-    ''' 
-    ###################### 수정 함수 ver1. 
-    def __iter__(self):
-        data_list = self.load_data_from_csv()
 
-        if not data_list:
-            print('----------------------------------------')
-            self.epoch_id += 1
-            print(self.epoch_id)
-            data_list = self.load_data_from_csv()
-
-        # Shuffle the data list
-        random.shuffle(data_list)
-
-        for batch_data in self.batch_data_generator(data_list):
-            yield batch_data
-    def batch_data_generator(self, data_list):
-        data_len = len(data_list)
-        idx = 0
-
-        while idx < data_len:
-            inputs, inputs_plus, inputs_minus, labels = [], [], [], []
-
-            for _ in range(self.bsz_one_step):
-                anchor, anchor_plus, anchor_minus = data_list[idx]
-                
-                # Tokenize the anchor text and add the EOS token
-                anchor_tokens = self.tokenizer.encode(anchor + self.eos_token)
-                anchor_plus_tokens = self.tokenizer.encode(anchor_plus + self.eos_token)
-                anchor_minus_tokens = self.tokenizer.encode(anchor_minus + self.eos_token)
-                
-
-                # Append to the respective lists
-                inputs.append(anchor_tokens[:-1])
-                inputs_plus.append(anchor_plus_tokens[:-1])
-                inputs_minus.append(anchor_minus_tokens[:-1])
-                labels.append(anchor_tokens[1:])  # You can modify this as needed
-
-                idx += 1
-            print(inputs[0])
-
-            yield (
-                torch.LongTensor(inputs),
-                torch.LongTensor(inputs_plus),
-                torch.LongTensor(inputs_minus),
-                torch.LongTensor(labels)
-            )
-    '''
-    ########################## 수정 ver2. (기존 유지)
     def __iter__(self):
         anchor_list, anchor_plus_list, anchor_minus_list = self.load_data_from_csv()
         
@@ -272,10 +170,10 @@ class EnglishPretrainCorpus:
         idx = 0
         s_idx, e_idx = 0, self.bsz_one_step
         while idx < batch_num:
-            inputs = [] ##################### 수정 
+            inputs = [] #
             for one_example_id in buffer_examples[s_idx:e_idx]:
                 inputs.append(one_example_id[:-1])
-                #labels.append(one_example_id[1:]) ################## 수정 
+                #labels.append(one_example_id[1:]) #
             assert len(inputs) == self.bsz_one_step
             s_idx += self.bsz_one_step
             e_idx += self.bsz_one_step
@@ -285,10 +183,10 @@ class EnglishPretrainCorpus:
         idx = 0
         s_idx, e_idx = 0, self.bsz_one_step
         while idx < batch_num:
-            inputs_plus, labels = [], []  ################## 수정 
+            inputs_plus, labels = [], []  #
             for one_example_id in buffer_examples_plus[s_idx:e_idx]:
                 inputs_plus.append(one_example_id[:-1])
-                labels.append(one_example_id[1:])  ################## 수정 
+                labels.append(one_example_id[1:])  #
             assert len(inputs_plus) == self.bsz_one_step
             s_idx += self.bsz_one_step
             e_idx += self.bsz_one_step
